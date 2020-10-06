1732dd72dbbf2eb9da7ace50309b846631a3e164
castanc <54039892+castanc@users.noreply.github.com>
2020-10-06 09:11:47 -0300

    Add files via upload

---
+++ UIUtils/frmClipboardCatcher.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace UIUtils
+{
+    partial class frmClipboardCatcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txMsg
+            // 
+            this.txMsg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txMsg.Enabled = false;
+            this.txMsg.Location = new System.Drawing.Point(0, 0);
+            this.txMsg.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txMsg.Multiline = true;
+            this.txMsg.Name = "txMsg";
+            this.txMsg.ReadOnly = true;
+            this.txMsg.Size = new System.Drawing.Size(338, 243);
+            this.txMsg.TabIndex = 0;
+            this.txMsg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmClipboardCatcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 243);
+            this.Controls.Add(this.txMsg);
+            this.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmClipboardCatcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clipboard Catcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmClipboardCatcher_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txMsg;
+    }
+}
---
+++ UIUtils/frmUserBase.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIUtils
+{
+    public partial class frmUserBase : Form
+    {
+        private frmClipboardCatcher frmCC = null;
+
+        public string WarningON { set; get; }
+        public string WarningOff { set; get; }
+        public string Warning
+        {
+            set { btnOpenMonitor.Text = value; }
+        }
+
+        public bool MonitorActive
+        {
+            get
+            {
+                return frmCC != null;
+            }
+        }
+
+        public frmUserBase()
+        {
+            WarningOff = "CLIPBOARD MONITOR IS OFF";
+            WarningON = "CLIPBOARD MONITOR IS ON";
+            InitializeComponent();
+            Warning = "WARNING. CLIPBOARD MONITOR IS ACTIVE";
+            refreshMonitorButtons();
+        }
+
+        public void HideMonitorForm()
+        {
+            if (frmCC != null)
+            {
+                frmCC.Hide();
+                frmCC.SendToBack();
+            }
+        }
+
+
+        private void refreshMonitorButtons()
+        {
+            if ( MonitorActive )
+            {
+                btnOpenMonitor.BackColor = Color.Red;
+                btnOpenMonitor.ForeColor = Color.White;
+                Warning = WarningON;
+            }
+            else
+            {
+                btnOpenMonitor.BackColor = Color.LightGray;
+                btnOpenMonitor.ForeColor = Color.Black;
+                Warning = WarningOff;
+            }
+        }
+
+        public void ShowMonitorForm()
+        {
+            if (frmCC != null)
+            {
+                frmCC.Show();
+                frmCC.BringToFront();
+            }
+
+        }
+
+        public virtual void OpenMonitorForm()
+        {
+            Clipboard.Clear();
+            if (frmCC == null)
+            {
+                frmCC = new frmClipboardCatcher();
+                frmCC.OnClipboardCapture += ClipboardCapture;
+                frmCC.OnClipboardCaptureImage += ClipboardCaptureImage;
+                frmCC.OnFormClosed += FrmCC_OnFormClosed;
+            }
+            frmCC.Show();
+            frmCC.BringToFront();
+            refreshMonitorButtons();
+        }
+
+        protected virtual void ClipboardCaptureImage(string fName)
+        {
+            
+        }
+
+        protected virtual void FrmCC_OnFormClosed()
+        {
+            frmCC.OnClipboardCapture -= ClipboardCapture;
+            frmCC.OnFormClosed -= FrmCC_OnFormClosed;
+            frmCC.Dispose();
+            frmCC = null;
+            refreshMonitorButtons();
+        }
+
+        public virtual void ClipboardCapture(string text)
+        {
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (frmCC != null)
+            {
+                frmCC.OnClipboardCapture -= ClipboardCapture;
+                frmCC.OnClipboardCaptureImage -= ClipboardCaptureImage;
+                frmCC.OnFormClosed -= FrmCC_OnFormClosed;
+            }
+        }
+
+        //shwo hide other applications
+        //https://social.msdn.microsoft.com/Forums/vstudio/en-US/9bde4870-1599-4958-9ab4-902fa98ba53a/how-do-i-maximizeminimize-applications-programmatically-in-c?forum=csharpgeneral
+        private void BtnOpenMonitor_Click(object sender, EventArgs e)
+        {
+            OpenMonitorForm();
+            //TODO have to find window by win api and show it
+        }
+
+    }
+}
